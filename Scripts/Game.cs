889e4eba3eead122ee82b5f28261c1994b136ab9
KeyJ148 <kejik148@gmail.com>
2023-12-17 15:36:23 +0300

    Use Game node for changing scenes

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -4,9 +4,19 @@ using System;
 
 public partial class Game : Node2D
 {
+	private Node _currentMainNode;
+
+	public void ChangeMainNode(Node mainNode)
+	{
+		_currentMainNode?.QueueFree();
+		_currentMainNode = mainNode;
+		AddChild(mainNode);
+	}
+	
 	public static Game Instance { get; private set; }
 	public override void _Ready()
 	{
 		Instance = this;
+		ChangeMainNode(GD.Load<PackedScene>("res://Scenes/MainMenu.tscn").Instantiate());
 	}
 }
--- Scripts/UI/Button.cs
+++ Scripts/UI/Button.cs
@@ -4,13 +4,12 @@ using System;
 public partial class Button : Godot.Button
 {
 	[Export] private PackedScene _scene;
+	
 	public override void _Ready()
 	{
 		Pressed += () =>
 		{
-			var world = _scene.Instantiate();
-			Game.Instance.AddChild(world);
-			QueueFree(); // Удолить меню
+			Game.Instance.ChangeMainNode(_scene.Instantiate());
 		};
 		
 	}
