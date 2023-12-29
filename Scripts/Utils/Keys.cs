c9718ab801eced915f2de5f90060e0fac483f054
Bronuh <bronyx@yandex.ru>
2023-12-29 09:31:05 +0300

    Add static references for input map

---
+++ Scripts/Utils/Keys.cs
@@ -0,0 +1,9 @@
+﻿namespace TOW.Scripts.Utils;
+
+public static class Keys
+{
+    public const string Forward = "key-forward";
+    public const string Back = "key-back";
+    public const string Left = "key-left";
+    public const string Right = "key-right";
+}
