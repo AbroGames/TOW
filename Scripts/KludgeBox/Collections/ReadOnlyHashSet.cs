3ed67ba56883508972695d2076bf7be94b6c12d2
Bronuh <bronyx@yandex.ru>
2023-12-29 12:32:01 +0300

    Add KludgeBox.Collections

---
+++ Scripts/KludgeBox/Collections/ReadOnlyHashSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOW.Scripts.KludgeBox.Collections;
+
+public class ReadOnlyHashSet<T> : IReadOnlyCollection<T>
+{
+	private readonly HashSet<T> _hashSet;
+
+	public ReadOnlyHashSet(HashSet<T> hashSet)
+	{
+		_hashSet = hashSet ?? throw new ArgumentNullException(nameof(hashSet));
+	}
+
+	public int Count => _hashSet.Count;
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		return _hashSet.GetEnumerator();
+	}
+
+	System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+	{
+		return _hashSet.GetEnumerator();
+	}
+}
