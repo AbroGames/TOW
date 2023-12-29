07a8379e56353c91003079e369d4ed00a018e26f
Bronuh <bronyx@yandex.ru>
2023-12-29 20:18:39 +0300

    Implement EventBus

---
+++ Scripts/Services/EventBus.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Godot;
+using TOW.Scripts.KludgeBox.Events;
+using TOW.Scripts.Services.ModLoader;
+using Bus = TOW.Scripts.KludgeBox.Events.EventBus;
+
+namespace TOW.Scripts.Services;
+
+
+public partial class EventBus : Service
+{
+    private Bus _bus = new Bus();
+
+    public EventBus()
+    {
+        _bus.IncludeBaseEvents = false;
+    }
+
+    public virtual void ScanEventListeners()
+    {
+        var listeners = EventScanner.ScanEventListeners();
+        _bus.SubscribeMethods(listeners);
+    }
+
+    /// <summary>
+    /// Subscribes a listener to the specified event type.
+    /// </summary>
+    /// <typeparam name="T">The event type to subscribe to.</typeparam>
+    /// <param name="action">The action to execute when the event is published.</param>
+    /// <returns>A listener token that can be used to unsubscribe from the event.</returns>
+    public virtual ListenerToken Subscribe<T>(Action<T> listener) where T : IEvent
+    {
+        return _bus.Subscribe(listener);
+    }
+
+    /// <summary>
+    /// Publishes an event to all registered listeners.
+    /// </summary>
+    /// <typeparam name="T">The type of event to publish.</typeparam>
+    /// <param name="event">The event to publish.</param>
+    public virtual void Publish<T>(T @event) where T : IEvent
+    {
+        _bus.Publish(@event);
+    }
+    
+    /// <summary>
+    /// Resets all the EventHubs.
+    /// </summary>
+    public virtual void Reset()
+    {
+        _bus.Reset();
+    }
+
+    /// <summary>
+    /// Resets the EventHub associated with the specified event type.
+    /// </summary>
+    /// <typeparam name="T">The event type for which to reset the EventHub.</typeparam>
+    public virtual void ResetEvent<T>() where T : IEvent
+    {
+        _bus.ResetEvent<T>();
+    }
+
+    internal override void Run()
+    {
+        ScanEventListeners();
+    }
+}
