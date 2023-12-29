497a237d63ce200608c7f5dc5022b4b4b2eabc58
Bronuh <bronyx@yandex.ru>
2023-12-29 20:18:23 +0300

    Add ServiceAttribute for automatic service registration

--- Scripts/ServiceAttribute.cs
+++ Scripts/ServiceAttribute.cs
@@ -3,4 +3,14 @@
 namespace TOW.Scripts;
 
 [AttributeUsage(AttributeTargets.Class)]
-public class ServiceAttribute : Attribute {}
+public class ServiceAttribute : Attribute
+{
+    public Type ServiceType { get; private set; } = null;
+
+    public ServiceAttribute() {}
+
+    public ServiceAttribute(Type serviceType)
+    {
+        ServiceType = serviceType;
+    }
+}
