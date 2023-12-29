71a07892a6fc9c3b3b85934c182cd684ec7f7ef9
Bronuh <bronyx@yandex.ru>
2023-12-29 12:18:27 +0300

    Add ColorExtensions as global static using

--- Scripts/KludgeBox/GlobalUsings.cs
+++ Scripts/KludgeBox/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using TOW.Scripts.KludgeBox.Godot;
 global using static TOW.Scripts.KludgeBox.Godot.Extensions.VectorExtensions;
+global using static TOW.Scripts.KludgeBox.Godot.Extensions.ColorExtensions;
