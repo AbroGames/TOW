c2b0a8ca7cb5e268c09478a53a8225c98fbc010a
Bronuh <bronyx@yandex.ru>
2023-12-29 10:27:02 +0300

    Implement basic Tank movement

--- Scripts/World/Tank.cs
+++ Scripts/World/Tank.cs
@@ -5,18 +5,46 @@ namespace TOW.Scripts.World;
 
 public partial class Tank : Node2D
 {
-	[Export] private double _moveSpeed = 100;
-	[Export] private double _rotationSpeed = 10;
+	[Export] private double _movementSpeed = 250; // in pixels/sec
+	[Export] private double _rotationSpeed = 120; // in angles/sec
+	[Export] private double _towerRotationSpeed = 240; // in angles/sec
 	private Tower Tower => GetNode("Tower") as Tower;
 	
-	public override void _Ready()
-	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 	public override void _Process(double delta)
 	{
+		var movementInput = GetInput();
+		var requiredTowerRotation = GetAimAngle();
+
+		// Movement
+		Position += (Vector2.Up.Rotated(Rotation)) * -movementInput.Y * _movementSpeed * delta;
+
+		// Tank rotation
+		Rotation += Mathf.DegToRad(movementInput.X * _rotationSpeed * delta);
+
+		// Tower rotation
+		var rotationDir = Mathf.Sign(requiredTowerRotation);
+		var absoluteRequiredRotation = Mathf.Abs(requiredTowerRotation);
+		Tower.Rotation += Mathf.DegToRad(rotationDir * Mathf.Clamp(_towerRotationSpeed * delta, -absoluteRequiredRotation, absoluteRequiredRotation));
+	}
+
+	private Vector2 GetInput()
+	{
+		return Input.GetVector(
+			Keys.Left, 
+			Keys.Right, 
+			Keys.Forward, 
+			Keys.Back).Sign();
+	}
+
+	private double GetAimAngle()
+	{
+		var mousePos = GetLocalMousePosition();
+		var mouseDir = Tower.Position.DirectionTo(mousePos);
+		var mouseAng = mouseDir.Angle();
+		var towerAng = Tower.Rotation;
+
 		
+		return Mathf.RadToDeg(Mathf.AngleDifference(towerAng, mouseAng) + Mathf.Pi/2);
 	}
 }
