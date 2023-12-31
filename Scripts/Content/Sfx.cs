c6b511dc4776443dc9f2c23d9835aeaf554c5e22
Bronuh <bronyx@yandex.ru>
2023-12-31 22:05:41 +0300

    Add Sfx references

---
+++ Scripts/Content/Sfx.cs
@@ -0,0 +1,13 @@
+﻿using TOW.Scripts.KludgeBox.Collections;
+
+namespace TOW.Scripts.Content;
+
+internal static class Sfx
+{
+    internal const string SoundsDir = "res://Assets/Audio/Sounds";
+		
+    // impacts
+    internal static RandomPicker<string> Hit { get; } = new RandomPicker<string>(
+        $"{SoundsDir}/MetalHeavyBashMetal{{0}}.wav".BatchNumber(1, 3)
+    );
+}
