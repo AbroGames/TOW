802d0f2634142df33360676d90f93d47e934e9db
Bronuh <bronyx@yandex.ru>
2024-01-01 01:02:39 +0300

    Add dash mechanic

--- Scripts/World/Tank.cs
+++ Scripts/World/Tank.cs
@@ -3,6 +3,7 @@ using KludgeBox.Godot;
 using TOW.Scripts.Content;
 using TOW.Scripts.Events;
 using TOW.Scripts.KludgeBox.Godot.Extensions;
+using TOW.Scripts.KludgeBox.Scheduling;
 using TOW.Scripts.Services;
 using TOW.Scripts.Services.ModLoader;
 using TOW.Scripts.Utils;
@@ -18,7 +19,10 @@ public partial class Tank : Node2D
 	private Tower Tower => GetNode("Tower") as Tower;
 	private Sprite2D Sprite => GetNode("Sprite2D") as Sprite2D;
 	private EventBus _eventBus => ServiceProvider.Get<EventBus>();
+	private bool _dashing = false;
 
+	private Cooldown _dashCd = new Cooldown(0.2, CooldownMode.Single);
+	private Cooldown _dashDuration = new Cooldown(0.5, CooldownMode.Single);
 	private double HitOpacity
 	{
 		get => (Sprite.Material as ShaderMaterial)?.GetShaderParameter("mixAmount").AsDouble() ?? -1;
@@ -27,13 +31,17 @@ public partial class Tank : Node2D
 	
 	public override void _Ready()
 	{
-		
+		_dashDuration.OnReady += () =>
+		{
+			_dashing = false;
+		};
 	}
 
 	public override void _Process(double delta)
 	{
 		HitOpacity = Mathf.Max(HitOpacity - 0.02, 0);
-		
+		_dashCd.Update(delta);
+		_dashDuration.Update(delta);
 		if(!_isPlayer) return;
 		
 		var movementInput = GetInput();
@@ -44,9 +52,11 @@ public partial class Tank : Node2D
 		if (movementInput.Y != 0)
 		{
 			var evt = new TankMovedEvent(this);
+			var speed = _dashing ? _movementSpeed * 3 : _movementSpeed;
+			
 			if (!_eventBus.PublishAndCheck(evt))
 			{
-				Position += this.Up() * movementInput.Y * _movementSpeed * delta;
+				Position += this.Up() * movementInput.Y * speed * delta;
 			}
 		}
 
@@ -79,7 +89,24 @@ public partial class Tank : Node2D
 		HitOpacity = 1;
 		Audio2D.PlaySoundOn((string)Sfx.Hit, this, 1);
 	}
-	
+
+	private int _num = 0;
+	public override void _Input(InputEvent @event)
+	{
+		if(Input.IsActionJustPressed(Keys.Forward))
+		{
+			if (_dashCd.FractionElapsed is < 1 and > 0)
+			{
+				_dashDuration.Restart();
+				_dashing = true;
+			}
+			else
+			{
+				_dashCd.Restart();
+			}
+		}
+	}
+
 	private Vector2 GetInput()
 	{
 		return Input.GetVector(
