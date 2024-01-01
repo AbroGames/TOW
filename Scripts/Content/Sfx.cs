3624f3aeed26c184f06911f9fe8a1cfbbe3462f4
Bronuh <bronyx@yandex.ru>
2024-01-01 21:25:31 +0300

    Add sample Sfx reference

---
+++ Scripts/Content/Sfx.cs
@@ -0,0 +1,10 @@
+﻿using TOW.Scripts.KludgeBox.Collections;
+
+namespace TOW.Scripts.Content;
+
+internal static class Sfx
+{
+	internal const string SoundsDir = "res://Assets/Audio/Sounds";
+
+	internal static string Explosion8Bit { get; } = $"{SoundsDir}/explosion_small_1.ogg";
+}
