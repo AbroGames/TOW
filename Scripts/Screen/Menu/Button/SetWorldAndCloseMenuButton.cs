b7f8e0838f890ea28dcfa72aa53ac96ec32b5f04
Bronuh <bronyx@yandex.ru>
2024-01-01 21:28:14 +0300

    Add test sfx for start button

--- Scripts/Screen/Menu/Button/SetWorldAndCloseMenuButton.cs
+++ Scripts/Screen/Menu/Button/SetWorldAndCloseMenuButton.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 using TOW.Scripts.Containers;
+using TOW.Scripts.Content;
+using TOW.Scripts.Services;
 
 namespace TOW.Scripts.Screen.Menu.Button;
 
@@ -17,6 +19,7 @@ public partial class SetWorldAndCloseMenuButton : Godot.Button
 	{
 		References.Instance.WorldContainer.ChangeStoredNode(_newWorldScene.Instantiate() as Node2D);
 		References.Instance.MenuContainer.ClearStoredNode();
+		ServiceProvider.Get<Audio>().PlayUiSound(Sfx.Explosion8Bit);
 	}
 
 }
