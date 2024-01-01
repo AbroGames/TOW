f4570da139d48e3fb1b22a0fb00accff378b76dd
Bronuh <bronyx@yandex.ru>
2024-01-01 21:26:02 +0300

    Add ExampleMod project

---
+++ ExampleMod/ExampleMod.cs
@@ -0,0 +1,12 @@
+﻿using TOW.Scripts.Services.ModLoader;
+using TOW.Scripts.Utils;
+
+namespace ExampleMod;
+
+public partial class ExampleMod : Mod
+{
+    public override void _Ready()
+    {
+        Log.Info("Example mod is loaded");
+    }
+}
---
+++ ExampleMod/StaticRunner.cs
@@ -0,0 +1,13 @@
+﻿using TOW.Scripts.Services.ModLoader;
+using TOW.Scripts.Utils;
+
+namespace ExampleMod;
+
+[StaticConstructorOnStartup]
+public static class StaticRunner
+{
+    static StaticRunner()
+    {
+        Log.Info("ExampleMod static constructor script executed");
+    }
+}
